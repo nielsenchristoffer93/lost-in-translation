e4cd2e68c60f137fb639862e6ac9ba2280fa32d4
Luis Gutierrez <luis.gutierrez@se.experis.com>
2021-08-16 17:00:56 +0200

    Fixed column/row design at Startup.js

--- src/components/Startup/Startup.js
+++ src/components/Startup/Startup.js
@@ -1,12 +1,49 @@
+import {Button, Col, Container, FormControl, InputGroup, Row} from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
+
 function Startup() {
 
     return (
+
         <main className="Startup">
-            <h1>Welcome to startup page</h1>
-            <p>What's your name?</p>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Row>
+                            <Col>Icon</Col>
+                            <Col>Welcome phrase</Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                                    <FormControl
+                                        placeholder="What's your name bitch? "
+                                        aria-label="Username"
+                                        aria-describedby="basic-addon1"
+                                    />
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Button variant="primary" >Go to translation</Button>
+                        </Row>
+
+                    </Col>
+                    <Col>
+                        --
+                    </Col>
+                </Row>
+
+            </Container>
         </main>
     )
 
 }
+
+
+
 
 export default Startup
