b970dd0af36bfbaeda413bca4556ea04e7af158b
danielDumville <daniel.dumville@se.experis.com>
2021-08-17 08:46:45 +0200

    Added Logout button in Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,11 +4,12 @@ import {Button, Col, Container, FormControl, InputGroup, Row} from "react-bootst
 function Profile() {
     const history = useHistory();
     const handleClick = () => history.push('/translation');
+    const handleClickLogOut = () => history.push('/'); //delete data too
     return(
         <main className="Profile">
             <h1>This is the user page</h1>
             <Button variant="primary" onClick={handleClick}>Translate again</Button>
-
+            <Button variant="primary" onClick={handleClickLogOut}>Logout</Button>
 
         </main>
     );
