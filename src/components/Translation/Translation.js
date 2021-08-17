5f360c1219ffbc7a12d41d317a03504268c3eb68
danielDumville <daniel.dumville@se.experis.com>
2021-08-17 08:58:56 +0200

    Added Translate button

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -4,10 +4,43 @@ import {Button, Col, Container, FormControl, InputGroup, Row} from "react-bootst
 function Translation() {
     const history = useHistory();
     const handleClick = () => history.push('/Profile');
+    const handleClickTranslate = () => console.log("TRANSLATED"); //placeholder
     return (
         <main className="Translation">
-            <h1>This is the translation page.</h1>
-            <Button variant="primary" onClick={handleClick}>Go to Profile </Button>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Row>
+                            <Col>Icon</Col>
+                            <Col>This is the translation page.</Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                                    <FormControl
+                                        placeholder="Translate something cool "
+                                        aria-label="Username"
+                                        aria-describedby="basic-addon1"
+                                    />
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                        <Button variant="primary" onClick={handleClickTranslate}>Translate </Button>
+                        <Button variant="primary" onClick={handleClick}>Go to Profile </Button>
+                        </Row>
+
+                    </Col>
+                    <Col>
+                        --
+                    </Col>
+                </Row>
+
+            </Container>
+            <h1></h1>
+            
         </main>
     );
 }
