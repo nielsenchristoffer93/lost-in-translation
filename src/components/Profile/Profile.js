7fa45bc285e4b2f3266472468dbb987bbad307d5
Christoffer Nielsen <christoffer.nielsen@se.experis.com>
2021-08-18 11:37:55 +0200

    Refactoring components and redirecting valid users from startup page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,6 +2,7 @@ import { Button, Card } from "react-bootstrap";
 import NavBar from "../hoc/NavBar";
 import CenterContainer from "../hoc/CenterContainer";
 import NavBarUser from "../hoc/NavBarUser";
+import CardTranslation from "../hoc/CardTranslation";
 
 function Translation() {
   return (
@@ -13,36 +14,9 @@ function Translation() {
         <div className="d-grid gap-2">
           <Button variant="dark">Clear Translations</Button>
         </div>
-        <Card className="card-container">
-          <Card.Header>Word</Card.Header>
-          <Card.Body>
-            <img
-              className="sign-img"
-              src={`/resources/individial_signs/${"c"}.png`}
-              alt="a.png"
-            />
-          </Card.Body>
-        </Card>
-        <Card className="card-container">
-          <Card.Header>Word</Card.Header>
-          <Card.Body>
-            <img
-              className="sign-img"
-              src={`/resources/individial_signs/${"e"}.png`}
-              alt="a.png"
-            />
-          </Card.Body>
-        </Card>
-        <Card className="card-container">
-          <Card.Header>Word</Card.Header>
-          <Card.Body>
-            <img
-              className="sign-img"
-              src={`/resources/individial_signs/${"d"}.png`}
-              alt="a.png"
-            />
-          </Card.Body>
-        </Card>
+        <CardTranslation stringToTranslate={"Test"}></CardTranslation>
+        <CardTranslation stringToTranslate={"Apa"}></CardTranslation>
+        <CardTranslation stringToTranslate={"Hejsan"}></CardTranslation>
       </CenterContainer>
     </main>
   );
